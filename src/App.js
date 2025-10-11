3a4daa1b2510f6d18c2ef4fa1fbe39e9f7bbc97a
user <renanmakoto@outlook.com>
2025-10-11 18:50:57 -0400

    Add names

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101536279 || Renan Yoshida Avelan || DevOps</h5>
+      <h5>Developed By: 101536279 || Renan Yoshida Avelan || COMP3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
